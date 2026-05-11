b108153b6618d980a897a5ca4e0ec345121104ff
hishantik <60609786+Hishantik@users.noreply.github.com>
2026-05-11 12:59:21 +0530

    Improve render.py: add timeout, better error handling, and log cleanup

--- script/render.py
+++ script/render.py
@@ -1,6 +1,7 @@
 import subprocess
 import vim
 import sys
+import os
 
 python_version = int(sys.version[0])
 
@@ -10,9 +11,11 @@ else:
     from urllib import request as requests
 
 render_server_process = None
+render_server_log = None
 url = vim.eval("g:render_server_path.':'.g:render_server_port")
 
 opener = requests.build_opener(requests.ProxyHandler({}))
+opener.addheaders = [('Connection', 'close')]
 
 
 def send(msg):
@@ -24,7 +27,8 @@ def send(msg):
         msg = msg.encode('utf-8')
 
     try:
-        opener.open(url, msg).read()
+        response = opener.open(url, msg, timeout=5)
+        response.read()
     except Exception as e:
         print("render error: " + str(e))
 
@@ -49,12 +53,13 @@ def startServer():
 
     log_path = vim.eval('g:render_server_log')
     if log_path is not None:
-        log_to = open(log_path, 'a')
+        render_server_log = open(log_path, 'a')
+        log_to = render_server_log
 
     try:
         render_server_process = subprocess.Popen(
             args,
-            cwd=vim.eval("s:plugin_path") + '/server',
+            cwd=vim.eval("s:plugin_path") or os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/server',
             stdout=subprocess.PIPE,
             stderr=log_to,
             stdin=subprocess.PIPE)
@@ -63,7 +68,7 @@ def startServer():
 
 
 def stopServer():
-    global render_server_process
+    global render_server_process, render_server_log
 
     if render_server_process is None:
         print('server not running')
@@ -72,3 +77,7 @@ def stopServer():
     render_server_process.terminate()
     render_server_process.wait()
     render_server_process = None
+
+    if render_server_log is not None:
+        render_server_log.close()
+        render_server_log = None
